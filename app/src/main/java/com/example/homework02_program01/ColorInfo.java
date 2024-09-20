47ce42c19cb7471f70d17cee6cbd965a1d476564
CecildeSist <arcetwo.com@gmail.com>
2024-09-20 16:22:18 -0400

    modified ColorInfo ints

--- app/src/main/java/com/example/homework02_program01/ColorInfo.java
+++ app/src/main/java/com/example/homework02_program01/ColorInfo.java
@@ -3,5 +3,5 @@ package com.example.homework02_program01;
 public class ColorInfo {
     //Store hex code, red progress, green progress, and blue progress
     private String hexaText;
-    int redProgress, greenProgress, blueProgress;
+    private int redProgress, greenProgress, blueProgress;
 }
