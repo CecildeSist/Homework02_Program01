01639082139149abd3dfbfae08d084e0c93ad681
CecildeSist <arcetwo.com@gmail.com>
2024-09-20 16:18:51 -0400

    created ints for progress bars in ColorInfo

--- app/src/main/java/com/example/homework02_program01/ColorInfo.java
+++ app/src/main/java/com/example/homework02_program01/ColorInfo.java
@@ -3,4 +3,5 @@ package com.example.homework02_program01;
 public class ColorInfo {
     //Store hex code, red progress, green progress, and blue progress
     private String hexaText;
+    int redProgress, greenProgress, blueProgress;
 }
